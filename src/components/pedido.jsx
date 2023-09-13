d52b207b7dfb08a9ba23b1a7b82618bbe080e13e
MARCOS <mago.dev.fasdatec@gmail.com>
2023-09-13 10:29:48 -0600

    agrege componentes

---
+++ src/components/pedido.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+function Pedido() {
+  return (
+    <div>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Logo" className="logo-img" />
+          <span className="logo-text">Tu Tienda</span>
+        </div>
+      </header>
+
+      <div className="App-header">
+        <div className="menu-container">
+          <h2>Pedidos</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Pedido;
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Orden from './ordenar';
 import Banner from './banner';
 import Contacto from './prueba';
-
+import Pedido from './pedido';
 export const AppRoutes = () => {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Banner />} />
       <Route path="/prueba" element={<Contacto />} />
       <Route path="/ordenar" element={<Orden />} />
+      <Route path="/pedido" element={<Pedido />} />
     </Routes>
+    </BrowserRouter>
   );
 };
