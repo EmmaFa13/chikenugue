4fb70b7de257bd8b92b6e3fb9a4f92bf5815c1b4
MARCOS <mago.dev.fasdatec@gmail.com>
2023-09-25 19:05:59 -0600

    "agrege footer un carrusel"

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Pedido from './components/pedido';
 import Login from './components/login';
 import Orden from './components/ordenar';
 import Footer from './components/footer';
+import Carrusel from './components/carrusel';
+import Footer1 from './components/footer1';
 function App() {
   return (
     <BrowserRouter>
@@ -23,7 +25,7 @@ function App() {
         <Login/>
         <Pedido />
       </div>
-      <Footer/>
+      <Footer1/>
     </BrowserRouter>
   );
 }
